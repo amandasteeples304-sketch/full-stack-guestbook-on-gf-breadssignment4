96833795ec786358b94e1ffcf66c3012141d07d0
Amanda Steeples <amandasteeples304@gmail.com>
2026-02-01 07:26:32 +0000

    try

--- sql.sql
+++ sql.sql
@@ -5,3 +5,36 @@ CREATE TABLE IF NOT EXISTS gfbread(
     opinion TEXT,
 );
 INSERT INTO gfbread (msg_name, bread_brand, opinion) VALUES ("Fiona", "All the brands", "Not all gf bread is bad, just enough to be a problem");
+
+[
+  {
+    "id": 1,
+    "bread": "Schar",
+    "opinion": "More holey than righteous, tastes of vinegar"
+  },
+  {
+    "id": 2,
+    "bread": "Warburtons pre-sliced loaves",
+    "opinion": "Slices are flatter than a pancake. Dry. Luckily a favourite of cafes, so you know the order is correct"
+  },
+  {
+    "id": 3,
+    "bread": "Warburtons bloomers",
+    "opinion": "No complaints, lasts for ages"
+  },
+  {
+    "id": 4,
+    "bread": "Genius",
+    "opinion": "Not worth the money, expires quickly"
+  },
+  {
+    "id": 5,
+    "bread": "Promise",
+    "opinion": "Worth the money, but you can only afford to eat one slice of bread with your sandwich"
+  },
+  {
+    "id": 6,
+    "bread": "Every single stores own brand",
+    "opinion": "Always dry, always tastes of mould, and all are so thinly sliced that a slice of ham is thicker"
+  }
+]
